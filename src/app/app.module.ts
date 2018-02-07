742bdfa3c50188e1e1f71c50a6ffd2900db6f78f
Hoang Le Minh <leminhhoang2509@gmail.com>
2018-02-07 03:47:09 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { YouTubeSearchComponent } from './you-tube-search/you-tube-search.component';
+import { youTubeSearchInjectables } from './you-tube-search/you-tube-search.injectables';
+import { SearchResultComponent } from './you-tube-search/search-result.component';
+import { SearchBoxComponent } from './you-tube-search/search-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YouTubeSearchComponent,
+    SearchResultComponent,
+    SearchBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    youTubeSearchInjectables
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
